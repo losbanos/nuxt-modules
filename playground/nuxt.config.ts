43625914dcfcedc9e5929a7a1b6a2542a9d48a90
kimtaehoon <kimtaehoon@smilegate.com>
2025-09-16 09:05:12 +0900

    add: tailwindcss

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,10 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ['../src/module', '@nuxtjs/tailwindcss'],
   imports: {
     autoImport: false
   },
   devtools: {enabled: true},
+  compatibilityDate: '2025-09-11',
   basicOptimizer: {
     dropConsole: true,
     nitroCompressPublicAssets: true,
--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,7 @@
 import {defineNuxtModule, createResolver, addPlugin} from '@nuxt/kit';
-import type {Nuxt, NuxtOptions} from 'nuxt/schema';
+import type {Nuxt, NuxtOptions, ViteConfig} from 'nuxt/schema';
 import type {NitroConfig} from 'nitropack';
 import {defu} from 'defu';
-import type {ViteConfig} from 'nuxt/schema';
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {
@@ -27,10 +26,9 @@ export default defineNuxtModule<ModuleOptions>({
     nitroMinify: true,
     disableUseAsyncDataDeep: false,
     manualChunks: {}
-    
   },
   hooks: {
-    "nitro:config": (nitroConfig: NitroConfig) => {
+    'nitro:config': (nitroConfig: NitroConfig) => {
       const {resolve} = createResolver(import.meta.url);
       nitroConfig.publicAssets ||= [];
       nitroConfig.publicAssets.push({
@@ -42,16 +40,16 @@ export default defineNuxtModule<ModuleOptions>({
   },
   setup(options: ModuleOptions, nuxt: Nuxt) {
     const nuxtOptions: NuxtOptions = nuxt.options;
-    
+
     const moduleOptions = defu(
-        nuxtOptions.runtimeConfig.public.basicOptimizer || {}, {
-          ...options
-        });
-        
+      nuxtOptions.runtimeConfig.public.basicOptimizer || {}, {
+        ...options
+      });
+
     nuxtOptions.runtimeConfig.public.basicOptimizer = moduleOptions;
-    
+
     if (moduleOptions.dropConsole) {
-      nuxt.hook('vite:extendConfig', (viteConfig, env) => {
+      nuxt.hook('vite:extendConfig', (viteConfig, _env) => {
         viteConfig.esbuild ||= {};
         viteConfig.esbuild.pure ||= [];
         viteConfig.esbuild.pure.push('console.log');
@@ -64,48 +62,48 @@ export default defineNuxtModule<ModuleOptions>({
         viteConfig.esbuild.pure ||= [];
         viteConfig.esbuild.pure.push('console.log');
       }
-      
+
       const chunks = Object.entries(moduleOptions.manualChunks ?? {}) as [string, Array<string>][];
       if (!chunks.length || !isClient || process.env.NODE_ENV !== 'production') return;
 
-      // @ts-ignore
+      // @ts-expect-error - viteConfig.build.rollupOptions.output is not typed in vite config
       viteConfig.build.rollupOptions.output.manualChunks = (id: string) => {
-        for(const [chunkName, chunkIds] of chunks) {
+        for (const [chunkName, chunkIds] of chunks) {
           for (const chunkId of chunkIds) {
             if (id.includes(chunkId)) {
               return chunkName;
             }
           }
         }
-      }
+      };
     });
-    
+
     nuxt.hook('nitro:config', (nitroConfig: NitroConfig) => {
       nitroConfig.compressPublicAssets = moduleOptions.nitroCompressPublicAssets;
       nitroConfig.minify = moduleOptions.nitroMinify;
     });
     nuxtOptions.experimental.defaults.useAsyncData.deep = !moduleOptions.disableUseAsyncDataDeep;
 
-    const fontLinks: Array<{rel: string, href: string, crossorigin?: "" | "anonymous" | "use-credentials"}> = [
+    const fontLinks: Array<{rel: string; href: string; crossorigin?: '' | 'anonymous' | 'use-credentials';}> = [
       {
-        rel: "preconnect",
-        href: "https://fonts.googleapis.com"
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com'
       },
       {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossorigin: "anonymous"
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: 'anonymous'
       },
       {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap'
       }
-    ]
-    fontLinks.forEach(link => {
+    ];
+    fontLinks.forEach((link) => {
       nuxtOptions.app.head.link?.push(link);
-    })
+    });
     const {resolve} = createResolver(import.meta.url);
-    nuxtOptions.css.push(resolve('./runtime/css/roboto.css'));
+    // nuxtOptions.css.push(resolve('./runtime/css/roboto.css'));
 
     addPlugin({src: resolve('./runtime/plugins/plugin')});
   }
